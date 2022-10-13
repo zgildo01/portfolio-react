29c4728d38b5e77e2c1187691ee8cb82835be3cc
zgildo01 <zzaragozagildo@gmail.com>
2022-10-13 19:26:33 -0400

    add basic styling to home page, change color of NavBar links

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,11 @@
+import './Home.css'
+
 const Home = () => {
   return (
     <>
-      <h1>Home</h1>
+      <div class="sign">
+      <span class="fast-flicker">Z</span>e<span class="flicker">u</span>s
+    </div>
     </>
   );
 }
