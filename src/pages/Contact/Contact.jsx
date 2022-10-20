a991582f99caeb840ae1a18691b0a54597c70f1f
zgildo01 <zzaragozagildo@gmail.com>
2022-10-20 14:11:20 -0400

    lay out basic skeleton of contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,7 +1,37 @@
+import './Contact.css'
+
+
 const Contact = () => {
   return (
     <>
-      <h1>Contact</h1>
+      <div id='main'>
+        <h1>Contact Me</h1>
+        <form id="fcf-form-id" class="fcf-form-class" method="post" action="contact-form-process.php">
+          <label for="Name" class="fcf-label">Your name</label>
+            <div class="fcf-input-group">
+              <input type="text" id="Name" name="Name" class="fcf-form-control" required />
+            </div>
+            <div class="fcf-form-group">
+          <label for="Email" class="fcf-label">Your email address</label>
+                <div class="fcf-input-group">
+                  <input type="email" id="Email" name="Email" class="fcf-form-control" required />
+                </div>
+            </div>
+          <div class="fcf-form-group">
+              <label for="Message" class="fcf-label">Your message</label>
+              <div class="fcf-input-group">
+                  <textarea id="Message" name="Message" class="fcf-form-control" rows="6" maxlength="3000" required></textarea>
+              </div>
+          </div>
+          <div class="fcf-form-group">
+              <button type="submit" id="fcf-button" class="fcf-btn fcf-btn-primary fcf-btn-lg fcf-btn-block">Send</button>
+          </div>
+        </form>
+        <div id='contact-icons'>
+          <img />
+          <img />
+        </div>
+      </div>
     </>
   );
 }
