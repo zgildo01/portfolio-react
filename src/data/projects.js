09f064323add4e7a80a1cd0a591bef190d4f79fb
zgildo01 <zzaragozagildo@gmail.com>
2022-11-08 11:35:09 -0500

    add projects data

---
+++ src/data/projects.js
@@ -0,0 +1,25 @@
+export const projects = [{
+  'title': 'Music Shelf',
+  'description': "A MEN stack social media esque-app that allows you to create albums, store them in your profile, and add songs to the album. You can comment on others' profiles and the owner can choose to delete comments if they please!",
+  'image': '../img/engulfment-screenshot.png',
+  'repositoryLink': 'https://github.com/zgildo01/The-Engulfment-Project',
+  'deploymentLink': 'https://engulfment-project.netlify.app/'
+}, {
+  'title': 'Engulfment Project',
+  'description': 'A simple HTML/CSS/JavaScript choose your own adventure game that puts you in the role of an FBI commander surviving through an unforgiving series of choices to escape a cult.',
+  'image': '../img/music-shelf-screenshot.png',
+  'repositoryLink': 'https://github.com/zgildo01/Music-Shelf',
+  'deploymentLink': 'https://music-shelf.fly.dev/'
+}, {
+  'title': 'Relocate',
+  'description': 'A fullstack MERN stack app created as a group project between Ryan Combest, Stephanie Michaels and I. This app allows you to create wishlists and todo-lists that help you in the process of moving from one home to another!',
+  'image': '../img/relocate-screenshot.png',
+  'repositoryLink': 'https://github.com/zgildo01/Relocate-front-end',
+  'deploymentLink': 'https://cgcorelocate.netlify.app/'
+}, {
+  'title': 'Car Collector',
+  'description': 'A simple django app with slight JavaScript functionality that allows you to have full create, read, update and delete functionality on cars you create as well as servicing lists for each car, and even car modifications!',
+  'image': '../img/engulfment-screenshot.png',
+  'repositoryLink': '',
+  'deploymentLink': ''
+}]
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,26 +1,17 @@
 import './Projects.css'
+import {projects} from '../../data/projects'
 
 const Projects = () => {
   return (
     <>
       <div id='main'>
-        <h1>Projects</h1>
-        <div id='project-card-container'>
-          <a href='https://music-shelf.fly.dev/' target='_blank' rel='noreferrer' className='project-card-anchor'>
-            <div id='project-card'>
-              <h4>Music Shelf</h4>
-              <p>A full CRUD application where users can share, create, edit and delete albums. Created using JavaScript, MongoDB, Mongoose, Node, HTML, CSS, EJS, Express</p>
-              <img src='../img/music-shelf-screenshot.jpg' alt='screenshot of a project Zeus made' />
-            </div>
-          </a>
-          <a href='https://music-shelf.fly.dev/' target='_blank' rel='noreferrer' className='project-card-anchor'>
-            <div id='project-card'>
-              <h4>The Engulfment Project</h4>
-              <p>A simple choose your own adventure game made with JavaScript, HTML and CSS.</p>
-              <img src='../img/engulfment-screenshot.jpg' alt='screenshot of a project Zeus made' />
-            </div>
-          </a>
-        </div>
+        <ul>
+        {projects.map((project) =>
+          <li key={project.title}>
+            {project.title}
+          </li>
+        )}
+        </ul>
       </div>
     </>
   );
