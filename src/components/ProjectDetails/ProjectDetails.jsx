2a0f65aa7724dfff437680a8870673b8936290b1
zgildo01 <zzaragozagildo@gmail.com>
2022-11-14 09:49:19 -0500

    finish styling project details page

--- src/components/ProjectDetails/ProjectDetails.jsx
+++ src/components/ProjectDetails/ProjectDetails.jsx
@@ -8,22 +8,28 @@ const ProjectDetails = () => {
   
   return (
     <>
-    <div id="main">
-      <h1>{project[0].title}</h1>
-      <div id="img-link">
-        <a href={project[0].deploymentLink}>
-          <h4>Click Me to Visit!</h4>
-          <img 
-            src={project[0].image} 
-            alt={project[0].title + " screenshot"} 
-          />
-        </a>
+      <div className={styles.main}>
+        <div className="sign">
+          <div className="fastflicker">
+            <h1 className={styles.title}>{project[0].title}</h1>
+          </div>
+        </div>
+        <div className={styles.aboutBody}>
+          <div className={styles.imgLink}>
+            <a className={styles.a} href={project[0].deploymentLink} target={"_blank"} rel='noreferrer'>
+              <img
+                className={styles.img}
+                src={project[0].image} 
+                alt={project[0].title + " screenshot"} 
+              />
+            </a>
+          </div>
+          <h2>{project[0].description}</h2>
+          <a className={styles.a} href={project[0].repositoryLink} target={"_blank"} rel='noreferrer'>
+            <img className={styles.icon} src='../img/githubPink.png' alt='github logo' />
+          </a>
+        </div>
       </div>
-      <h2>{project[0].description}</h2>
-      <a href={project[0].repositoryLink}>
-        <h3>Click me to visit the repository!</h3>
-      </a>
-    </div>
     </>
   );
 }
